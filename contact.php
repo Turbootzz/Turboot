57f988511fb8c00aa9103b068f05047b1cc73d01
Thijs Herman <tc.herman@hotmail.com>
2022-11-21 11:52:49 +0100

    Migrating to EmailJS (start)

--- contact.php
+++ contact.php
@@ -45,7 +45,14 @@ include_once "header.php";
 </div>
 
 </div>
-
+<script type="text/javascript"
+        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js">
+</script>
+<script type="text/javascript">
+   (function(){
+      emailjs.init("1fonBJy85TkzG1Jje");
+   })();
+</script>
 <?php
 include_once "footer.php";
 ?>
