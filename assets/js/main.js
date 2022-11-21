7afe875e18b29cf057cdc91b0ae09325a8fb2ab1
Thijs Herman <tc.herman@hotmail.com>
2022-11-21 12:08:14 +0100

    EmailJS (V2)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
 console.log("Turboot Website");
+
+
+// EmailJS
+(function(){
+    emailjs.init("1fonBJy85TkzG1Jje");
+ })();
+
+ var templateParams = {
+    name: 'Turboot',
+    notes: 'Check test'
+};
+ 
+emailjs.send('service_iq9eq5d', 'template_bwxadoi', templateParams)
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
