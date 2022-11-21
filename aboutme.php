608de95623e5306419002955dc8ccf131760ebac
Thijs Herman <tc.herman@hotmail.com>
2022-11-21 22:38:49 +0100

    update about me page

--- aboutme.php
+++ aboutme.php
@@ -8,15 +8,15 @@ include_once "header.php";
   <h1>About me</h1>
   <div class="aboutme-cards">
     <div class="aboutme-card"><a class="cardlink" href="https://github.com/Turbootzz/webshop1">
-        <img class="cardimg" src="assets/img/phonium1.png" alt="Phonium Webshop" style="width:100%">
+        <img class="aboutme-cardimg" src="assets/img/me1.png" alt="Phonium Webshop" style="height: 500px;">
         <div class="aboutme-cardtext">
-          <h4><b>Phonium Webshop</b></h4>
-          <p>Made in PHP. A fully functioning webshop. Click to view.</p>
+          <h4><b>Who am I?</b></h4>
+          <p>My name is Thijs Herman, I am a junior web developer with experience in multiple languages and libraries.</p>
         </div>
       </a>
     </div>
-    <div class="aboutme-card"><a class="cardlink" href="projects/pong">
-        <img class="cardimg" src="assets/img/pong1.png" alt="The Pong Game" style="width:100%">
+    <!-- <div class="aboutme-card"><a class="cardlink" href="projects/pong">
+        <img class="aboutme-cardimg" src="assets/img/pong1.png" alt="The Pong Game" style="height: 500px;">
         <div class="aboutme-cardtext">
           <h4><b>Pong</b></h4>
           <p>Made in Javascript. You play against an AI and it uses physics. Click to play.</p>
@@ -24,13 +24,13 @@ include_once "header.php";
       </a>
     </div>
     <div class="aboutme-card"><a class="cardlink" href="https://github.com/Turbootzz/SystemTH">
-        <img class="cardimg" src="assets/img/systemth1.png" alt="SystemTH Discord Bot" style="width:100%">
+        <img class="aboutme-cardimg" src="assets/img/systemth1.png" alt="SystemTH Discord Bot" style="height: 500px;">
         <div class="aboutme-cardtext">
           <h4><b>SystemTH Discord Bot</b></h4>
           <p>Made in Python. A bot with multiple functionalities. Click to view.</p>
         </div>
       </a>
-    </div>
+    </div> -->
   </div>
 </div>
 <div class="spacing">
