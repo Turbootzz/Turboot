71fd2abc3ced78f0476684c121ab52bf7c75793c
Thijs Herman <tc.herman@hotmail.com>
2023-02-23 23:59:31 +0100

    Added EmailJS
    Contact page now fulle working.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,22 @@
 console.log("Turboot Website");
 
-
 // EmailJS
-(function(){
-    emailjs.init("1fonBJy85TkzG1Jje");
- })();
 
- var templateParams = {
-    name: 'Turboot',
-    notes: 'Check test'
-};
- 
-emailjs.send('service_iq9eq5d', 'template_bwxadoi', templateParams)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
+var PUBLIC_KEY = config.PUBLIC_KEY;
+var SERVICE_ID = config.SERVICE_ID;
+var TEMPLATE_ID = config.TEMPLATE_ID;
+(function () {
+   emailjs.init(PUBLIC_KEY);
+})();
+
+function sendMail() {
+   var params = {
+      name : document.getElementById("name").value,
+      email : document.getElementById("email").value,
+      subject : document.getElementById("subject").value,
+      message : document.getElementById("message").value,
+   }
+   emailjs.send(SERVICE_ID, TEMPLATE_ID, params).then(function (res) {
+      alert("Mail succesfully sent! " + res.status);
+   })
+}
--- contact.php
+++ contact.php
@@ -1,51 +1,46 @@
 <?php
 include_once "header.php";
 ?>
- 
- <div class="contact-body">
-
-<div id="contact-form">
-<h3 class="contact-h3">Contact us</h3>
-
-<form id="contact-form-id" class="contact-form-class" method="post" action="contact-process.php">
-    
-    <div class="contact-form-group">
-        <label for="name" class="contact-label">Your name</label>
-        <div class="contact-input-group">
-            <input type="text" id="Name" name="name" class="contact-form-control" required>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js">
+</script>
+<script type="text/javascript" src="assets/js/config.js"></script>
+<div class="contact-body">
+    <div id="contact-form">
+        <h3 class="contact-h3">Contact us</h3>
+        <div id="contact-form" class="contact-form-class">
+            <input type="hidden" name="contact_number">
+            <div class="contact-form-group">
+                <label for="name" class="contact-label">Your name</label>
+                <div class="contact-input-group">
+                    <input type="text" id="name" name="name" class="contact-form-control" required>
+                </div>
+            </div>
+            <div class="contact-form-group">
+                <label for="mail" class="contact-label">Your email address</label>
+                <div class="contact-input-group">
+                    <input type="email" id="email" name="email" class="contact-form-control" required>
+                </div>
+            </div>
+            <div class="contact-form-group">
+                <label for="subject" class="contact-label">Subject</label>
+                <div class="contact-input-group">
+                    <input id="subject" name="subject" class="contact-form-control" required>
+                </div>
+            </div>
+            <div class="contact-form-group">
+                <label for="message" class="contact-label">Message</label>
+                <div class="contact-input-group">
+                    <textarea id="message" name="message" class="contact-form-control" rows="6" maxlength="3000"
+                        required></textarea>
+                </div>
+            </div>
+            <div class="contact-form-group">
+                <button onclick="sendMail()" type="submit" id="submit-button"
+                    class="contact-btn contact-btn-primary contact-btn-lg contact-btn-block">Send Message</button>
+            </div>
         </div>
     </div>
-
-    <div class="contact-form-group">
-        <label for="mail" class="contact-label">Your email address</label>
-        <div class="contact-input-group">
-            <input type="email" id="Email" name="mail" class="contact-form-control" required>
-        </div>
-    </div>
-
-    <div class="contact-form-group">
-        <label for="subject" class="contact-label">Subject</label>
-        <div class="contact-input-group">
-            <input id="Message" name="subject" class="contact-form-control" required>
-        </div>
-    </div>
-
-    <div class="contact-form-group">
-        <label for="message" class="contact-label">Message</label>
-        <div class="contact-input-group">
-            <textarea id="Message" name="message" class="contact-form-control" rows="6" maxlength="3000" required></textarea>
-        </div>
-    </div>
-
-    <div class="contact-form-group">
-        <button type="submit" name="submitmail" id="contact-button" class="contact-btn contact-btn-primary contact-btn-lg contact-btn-block">Send Message</button>
-    </div>
-
-</form>
 </div>
-
-</div>
-
 <?php
 include_once "footer.php";
 ?>
