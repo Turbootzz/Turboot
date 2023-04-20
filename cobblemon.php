e4cd78a7681ada18595cb9d914e8439b5b1e0baa
Thijs Herman <tc.herman@hotmail.com>
2023-04-20 21:15:33 +0200

    cobblemon tweaks

--- cobblemon.php
+++ cobblemon.php
@@ -1,76 +1,77 @@
 <!doctype html>
-<html class="no-js" lang="">
-
+<html class="no-js" lang="en">
 <head>
-  <meta charset="utf-8">
-  <title>Turboot - Pixelmon</title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <meta property="og:title" content="">
-  <meta property="og:type" content="">
-  <meta property="og:url" content="">
-  <meta property="og:image" content="">
-
-  <link rel="manifest" href="site.webmanifest">
-  <link rel="apple-touch-icon" href="icon.png">
-  <link rel="stylesheet" href="assets/css/cobblemon.css">
-
-  <meta name="theme-color" content="#fafafa">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">
+    <meta charset="utf-8">
+    <title>Turboot - Pixelmon</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="icon.png">
+    <link rel="stylesheet" href="assets/css/cobblemon.css">
+    <meta name="theme-color" content="#fafafa">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">
 </head>
 <body>
-  <header>
-  <ul class="topnav">
-    <li><a href="/">Home</a></li>
-    <li><a href="">Shop</a></li>
-    <li><a class="active" href="#">Download</a></li>
-    <li class="right"><a href="/">Turboot Website</a></li>
-  </ul>
-</header>
-<main>
-<br>
-<hr>
-<br>
-    <h3>Download Cobblemon Modpack</h3>
-  <a href="assets/downloads/modpack-bf-cobblemon-1.19.2.zip"><button class="btn" style="width:100%"><i class="fa fa-download"></i> Download</button></a>
-  <br>
-  <br>
-  <br>
-  <br>
-<hr>
-<br>
-  <div id="left">
-  <ul>
-    <h4>Instructions</h4>
-    <li>Download the zip, go to the place where it is downloaded.</li>
-    <li>Press right-click on it and unzip it.</li>
-    <li>Open the folder you just unzipped.</li>
-    <li>Open the fabric-installer(version).jar file and install it in your client.</li>
-    <li>To download the mods, go to file explorer and type in the address bar: %appdata%</li>
-    <li>After doing that you should open the .minecraft folder</li>
-    <li>You should see a folder named: "mods", if you don't, just make one.</li>
-    <li>Make sure the mods folder is empty.</li>
-    <li>Go back to the zip and copy the content in the mods folder.</li>
-    <li>At last go back again to the .minecraft directory and paste the content in the mods folder.</li>
-  </ul>
-  <p style="margin-left: 10px;"> If you do not get my poor explenation just <a href="https://discord.gg/Z9dZC6V7cQ">click here</a> to join our discord and I'll help you out :)</p>
-</div>
-  <ul id="right">
-    <h4>Requirements</h4>
-    <li>You will need <a href="https://download.oracle.com/java/17/archive/jdk-17.0.7_windows-x64_bin.exe">Java</a> to be able to run the fabric.jar file. (Java 17 is recommended)</li>
-    <li>Need to have <a href="https://www.minecraft.net/download/" download>Minecraft (Java Edition)</a> installed ofcourse.</li>
-<br>
-    <h4>Additional Mods</h4>
-    <li>Download: <a href="" download>none</a></li>
-    <br>
-    <h4>Tips</h4>
-    <li>Improve your minecraft performance by locating more RAM.</li>
-  </ul>
-</main>
-<footer>
-
-</footer>
+    <header>
+        <ul class="topnav">
+            <li><a href="/">Home</a></li>
+            <li><a href="">Shop</a></li>
+            <li><a class="active" href="#">Download</a></li>
+            <li class="right"><a href="/">Turboot Website</a></li>
+        </ul>
+    </header>
+    <main>
+        <br>
+        <hr>
+        <br>
+        <h3>Download Cobblemon Modpack</h3>
+        <a href="assets/downloads/modpack-bf-cobblemon-1.19.2.zip"><button class="btn" style="width:100%"><i
+                    class="fa fa-download"></i> Download</button></a>
+        <br>
+        <br>
+        <br>
+        <br>
+        <hr>
+        <br>
+        <div id="left">
+            <ul>
+                <h4>Instructions</h4>
+                <li>Download the zip, go to the place where it is downloaded.</li>
+                <li>Press right-click on it and unzip it.</li>
+                <li>Open the folder you just unzipped.</li>
+                <li>Open the fabric-installer(version).jar file and install the 1.19.2 version in your client.</li>
+                <li>To download the mods, go to file explorer and type in the address bar: %appdata%</li>
+                <li>After doing that you should open the .minecraft folder</li>
+                <li>You should see a folder named: "mods", if you don't, just make one.</li>
+                <li>Make sure the mods folder is empty.</li>
+                <li>Go back to the zip and copy the content in the mods folder.</li>
+                <li>At last go back again to the .minecraft directory and paste the content in the mods folder.</li>
+            </ul>
+            <p style="margin-left: 10px;"> If you do not get my poor explenation just <a
+                    href="https://discord.gg/Z9dZC6V7cQ">click here</a> to join our discord and I'll help you out :)</p>
+        </div>
+        <ul id="right">
+            <h4>Requirements</h4>
+            <li>You will need <a
+                    href="https://download.oracle.com/java/17/archive/jdk-17.0.7_windows-x64_bin.exe">Java</a> to be
+                able to run the fabric.jar file. (Java 17 is recommended)</li>
+            <li>Need to have <a href="https://www.minecraft.net/download/" download>Minecraft (Java Edition)</a>
+                installed ofcourse.</li>
+            <br>
+            <h4>Information</h4>
+            <li>The reason we use fabric instead of forge is because of<br>better performance and more functionality.
+            </li>
+            <br>
+            <h4>Tips</h4>
+            <li>Improve your minecraft performance by locating more RAM.</li>
+        </ul>
+    </main>
+    <footer>
+    </footer>
 </body>
 </html>
